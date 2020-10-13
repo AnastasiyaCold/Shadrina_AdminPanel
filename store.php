adb5906b2b88b74ccb898a70073630cd61c51195
Anastasiya <48973322+AnastasiyaCold@users.noreply.github.com>
2020-10-13 11:50:18 +0700

    Delete store.php

--- store.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'db.php';
-$name = $_POST['name'];
-$description = $_POST['description'];
-$price = $_POST['price'];
-$category = $_POST['category'];
-$pdo->query('INSERT INTO `product` (`name`,`description`,`price`,`category`)
-VALUES ("'.$name.'","'.$description.'","'.$price.'","'.$category.'")');
-header('Location: pagetwo.php');
