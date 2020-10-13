0a67f343be9851a13b0ecd1747f9e80e1b0906fd
Anastasiya <48973322+AnastasiyaCold@users.noreply.github.com>
2020-10-13 11:49:10 +0700

    Delete upd.php

--- upd.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'db.php';
-$pdo->query('UPDATE `product` SET `name` = "'.$_POST['name'].'",
-`description` = "'.$_POST['description'].'",`price` = "'.$_POST['price'].'",
-`category` = "'.$_POST['category'].'" WHERE `id`='.$_POST['id']);
-header('Location: pagetwo.php');
